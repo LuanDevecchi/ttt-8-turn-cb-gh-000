898b082e35d87de6a6e81d4a62889221f4df85c2
Luan Devecchi <gitluan@engineer.com>
2018-08-27 23:41:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,13 +2,9 @@ def makeplay(x)
   return " #{x} "
 end
 
-# code your #valid_move? method here
-
 def valid_move?(board,index)
    !position_taken?(board,index) && index.between?(0,8)
 end
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board,index)
   board[index] != " " && board[index] != "" && board[index] != nil
